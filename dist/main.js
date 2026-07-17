e305ad0d7118b5dcaff0d1b621e5cfb12e3a94ab
MALIKABDULAHAWAN <u2022274@giki.edu.pk>
2026-07-18 02:34:11 +0500

    2

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    await app.listen(process.env.PORT || 3000);
+    console.log('Application is running on: ' + await app.getUrl());
+}
+bootstrap().catch(err => {
+    console.error('Failed to bootstrap:', err);
+    process.exit(1);
+});
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+﻿import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+  console.log('Application is running on: ' + await app.getUrl());
+}
+bootstrap().catch(err => {
+  console.error('Failed to bootstrap:', err);
+  process.exit(1);
+});
